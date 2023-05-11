71b857fdbc50b018eb5cad17ab54a0e7d7b50078
jackhzinn <math.zinn@gmail.com>
2023-05-11 14:28:29 -0400

    Save working version of RoboFriends

---
+++ src/robots.js
@@ -0,0 +1,49 @@
+export const robots = [
+    {
+        id:1,
+        name: "Lenny Hoffsteader",
+        username: 'Lenny',
+        email: "lhoffs@robot.com"
+    },
+    {
+        id:2,
+        name: "Penny Unknown",
+        username: "penny",
+        email: "penny@robot.com"
+    },
+    {
+        id:3,
+        name: "Howard Wolowitz",
+        username: "howie",
+        email: "rocketman@robot.com"
+    },
+    {
+        id:4,
+        name: "Sheldon Cooper",
+        username: "shelly",
+        email: "scooper@robot.com"
+    },
+    {
+        id:5,
+        name: "Amy Farah Fowler",
+        username: "amyff",
+        email: "amyff@robot.com"
+    },
+    {
+        id:6,
+        name: "Raj Kuthrapali",
+        username: "rajesh",
+        email: "rajesh@robot.com"
+    },
+    {
+        id:7,
+        name: "Bernadette Rostenkowski",
+        username: "bernie",
+        email: "bernie@robot.com"
+    }
+
+
+
+
+
+];
