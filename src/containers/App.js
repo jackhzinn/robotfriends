099ccad0994785971b273938c9e4849aad0028b7
jackhzinn <math.zinn@gmail.com>
2023-05-11 15:59:52 -0400

    Reorganized into folders

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-import CardList from "./CardList";
-import SearchBox from "./SearchBox";
-
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
 import './App.css';
 
 class App extends React.Component {
@@ -33,7 +33,9 @@ class App extends React.Component {
                     <div className="tc">
                         <h1 className="f1 light-green">RoboFriends</h1>
                         <SearchBox searchChange={this.onSearchChange}/>
-                        <CardList robots={filteredRobots} />
+                        <Scroll>
+                            <CardList robots={filteredRobots} />
+                        </Scroll>
                     </div>
                 </React.StrictMode>
             )
